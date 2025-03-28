dd9dd7a6a2709b2307f1aa5c161a7bbae28aa66e
byprakosoo <bayuprakoso.adi@gmail.com>
2025-03-28 14:55:34 +0700

    comment nullable

--- app/Request/InsertCommentRequest.php
+++ app/Request/InsertCommentRequest.php
@@ -17,7 +17,7 @@ class InsertCommentRequest extends Form
             'id' => ['nullable', 'str', 'trim', 'uuid', 'max:37'],
             'name' => ['required', 'str', 'trim', 'min:1', 'max:40'],
             'presence' => ['bool'],
-            'comment' => ['required', 'str', 'min:1', 'max:1000'],
+            'comment' => ['nullable', 'str', 'min:1', 'max:1000'],
             'ip' => ['nullable', 'str', 'trim', 'max:50'],
             'user_agent' => ['nullable', 'str', 'trim', 'max:500']
         ];
